1f89329052c9c30d6318758b4da0ba6302c99b86
Jaewon <j901207@gmail.com>
2022-03-10 00:59:14 +0900

    bottle entity update

--- models/entity/bottle.go
+++ models/entity/bottle.go
@@ -7,6 +7,10 @@ import (
 type Bottle struct {
 	mgm.DefaultModel `bson:",inline" swaggerignore:"true"`
 	Title string `json:"title" example:"default" binding:"require"`
+	Index string `json:"index" example:"1" binding:"require"`
+	Type string `json:"type" example:"1" binding:"require"`
+	IsOpen bool `json:"isOpen" example:"false" binding:"require"`
+	ImgUri string `json:"imgUri" example:"any.img.uri" binding:"require"`
 	Description string `json:"description" example:"" binding:"require"`
 	Maturity_date string `json:"maturityDate" example:"" binding:"require"`
 	UserId string `json:"userId" example:"Object ID" swaggerignore:"true"`
