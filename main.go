f18ef29ce61bf2cfc1b6cb636ae403d7684b9b67
Jaewon <j901207@gmail.com>
2022-03-09 23:30:56 +0900

    swag baseurl

--- main.go
+++ main.go
@@ -28,11 +28,27 @@ import (
 // @securityDefinitions.apiKey ApiKeyAuth
 // @in header
 // @name Authorization
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization, Origin")
+		// c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET, DELETE, POST")
 
+		if c.Request.Method == "OPTIONS" {
+				c.AbortWithStatus(204)
+				return
+		}
+
+		c.Next()
+	}
+}
 func main (){
 	utils.GetENV()
 	db.SetDatabase()
 	r := router.SetRouter()
+	r.Use(CORSMiddleware())
+
 	docs.SwaggerInfo.BasePath = ""
 
 	r.GET("/",func (c *gin.Context) {
